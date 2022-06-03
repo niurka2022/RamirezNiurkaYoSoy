b5fe2859d61da7fb1a374b294efec662af217c3d
niurka2022 <niurka2022>
2022-06-02 21:05:22 -0500

    Cree el programa menu

---
+++ menu.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+        int op;
+        do{
+                cout<<"1. Suma varios numero";
+                cout<<"2. Compara";
+                cout<<"3. Salir";
+                cin>>op;
+        }while(op !=3);
+
+        return 0;
+}
