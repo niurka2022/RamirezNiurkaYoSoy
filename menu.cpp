ef1ee9bcbd241f54e73c7d92d258ec2da765a30c
niurka2022 <niurka2022>
2022-06-02 21:44:10 -0500

    Actualizacion de mi menu

--- menu.cpp
+++ menu.cpp
@@ -1,14 +1,20 @@
 #include<iostream>
 using namespace std;
-int main ()
-{
-        int op;
-        do{
-                cout<<"1. Suma varios numero";
-                cout<<"2. Compara";
-                cout<<"3. Salir";
-                cin>>op;
-        }while(op !=3);
 
+int main ()                                           {                                                             int op,flag;
+                                                              do{
+                cout<<"1. Suma varios numero \n";
+                cout<<"2. Compara \n";                                cout<<"3. Salir\n\n";
+                cin>>op;                                      switch(op)
+        {
+                case 1:                                                       cout<<"Aqui va la suma de varios numero";
+                        flag=cin.ignore().get();
+                        break;                                        case 2:                               
+                        cout<<"Aqui va el programa compara ";                                                                       flag=cin.ignore().get();                              break;
+                default:
+                        cout<<"Esta opcion no es computable ";
+                        flag=cin.ignore().get();
+        }
+        }while(op !=3);
         return 0;
 }
